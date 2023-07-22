f5a0482985e67c64e5c106a7f2bfafe9d4636976
Rushi <ihsur7@gmail.com>
2023-07-22 16:11:43 +1000

    Delete costing.py
    - removed, unnecessary.

--- costing.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-#create a pricing dataframe
-pricing = pd.DataFrame([['Piping (per mm)', 0.04],['L Joint', 1], ['T Joint', 1.1]], columns=['Item', 'Price (per unit)'])
-
-print(pricing.head())
